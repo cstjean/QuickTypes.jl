ff1997c449386dbf7974e03beb02b202a94e435a
Cedric St-Jean <cedric.stjean@gmail.com>
2016-10-08 12:29:13 -0400

    Wrote tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using QuickTypes
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+abstract Vehicle
+
+@qtype Car{T<:Number, U}(size::T, nwheels::Int=4; manufacturer::U=nothing,
+                         brand::String="off-brand") <: Vehicle
+
+c = Car(10; manufacturer=("Danone", "Hershey"))
+
+@test c.size==10
+@test c.nwheels==4
+@test c.manufacturer==("Danone", "Hershey")
+@test c.brand=="off-brand"
+# Check that the fields are in the right order
+@test fieldnames(c) == [:size, :nwheels, :manufacturer, :brand]
+
+################################################################################
+
+@qimmutable Empty()
+Empty()
+
+################################################################################
+
+@qimmutable Kwaroo(x; y=10)
+@test Kwaroo(5) == Kwaroo(5; y=10)
